a2b7dce978b3f2e3d16db15dda444d48b4c02a85
ceru <cerulean126@gmail.com>
2021-08-21 23:58:12 +0900

    models.py group 모델 외래키 User로 수정 admin.py inline수정

--- cspc_recog_server/users/models.py
+++ cspc_recog_server/users/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 class Group(models.Model):
     group_name = models.CharField(max_length=100)
     #group_admin_id = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    group_admin_id = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.group_name
 
 class Profile(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
-    group_id = models.ForeignKey(Group,on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
     nick_name = models.CharField(max_length=100)
     last_visit_time = models.DateTimeField(auto_now=True)  # 저장시 자동 시간 업데이트
     is_online = models.BooleanField(default=False)
