7919d352c0643beaa64625665fde85da6a2a041b
ceru <cerulean126@gmail.com>
2021-08-23 20:42:05 +0900

    프로필 리스트 가져오는 부분 추가함

--- cspc_recog_server/users/models.py
+++ cspc_recog_server/users/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
+
+def profileImageUpload(instance, filename):
+    return 'image/profile/{0}/{1}'.format(instance.id,filename)
+
 
 class Group(models.Model):
     group_name = models.CharField(max_length=100)
@@ -14,8 +19,10 @@ class Profile(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
     nick_name = models.CharField(max_length=100)
     last_visit_time = models.DateTimeField(auto_now=True)  # 저장시 자동 시간 업데이트
+    visit_time_sum = models.DurationField(default=datetime.timedelta())
     is_online = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    profile_image = models.ImageField(upload_to = profileImageUpload,blank=True)
 
     def __str__(self):
         return self.nick_name
