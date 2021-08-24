bfd7137d6722f7f65dcbd2d73db48e605edd10f9
Sumin Kim <dobbie6174@gmail.com>
2021-08-25 03:15:38 +0900

    add calendar put api

--- cspc_recog_server/calendars/urls.py
+++ cspc_recog_server/calendars/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 urlpatterns = [
     path("all/", calendarAPI),
-    path("<int:pk>/event/", eventAPI),
-    path("<int:pk>/event/post/",eventPostAPI),
-    path("<int:cal_id>/event/<int:event_id>/delete/",eventDeleteAPI),
+    path("<int:pk>/event/", EventAPIView.as_view()),
+    path("<int:pk>/event/post/",EventAPIView.as_view()),
+    path("<int:cal_id>/event/<int:event_id>/delete/",EventAPIView.as_view()),
+    path("<int:cal_id>/event/<int:event_id>/edit/", EventAPIView.as_view()),
 ]
--- cspc_recog_server/calendars/views.py
+++ cspc_recog_server/calendars/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .models import Calendar, Event
 from .serializers import CalendarSerializer, EventSerializer
 import json
@@ -10,36 +11,44 @@ def calendarAPI(request):
     serializer = CalendarSerializer(all_calendar, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def eventAPI(request, pk):
-    all_event = Event.objects.filter(calendar_id = pk).order_by('date')
-    serializer = EventSerializer(all_event, many=True)
-    return Response(serializer.data)
+class EventAPIView(APIView):
+    def get(self, request, pk):
+        all_event = Event.objects.filter(calendar_id = pk).order_by('date')
+        serializer = EventSerializer(all_event, many=True)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def eventPostAPI(request, pk):
-    if request.method == 'POST':
-        
-        calendar_id = Calendar.objects.filter(id = pk)[0]
+    def post(self, request, pk):
+        if request.method == 'POST':
+            
+            calendar_id = Calendar.objects.filter(id = pk)[0]
+            
+            request_data = json.loads(request.body.decode('utf-8'))[0]
+            title = request_data['title']
+            description = request_data['description']
+            date = request_data['date']
+            
+            event = Event(calendar_id = calendar_id,
+            title = title, description = description, date = date
+            )
+            event.save()
+            
+            return Response(True)
 
-        print(json.loads(request.body))
-        
-        request_data = json.loads(request.body.decode('utf-8'))[0]
-        title = request_data['title']
-        description = request_data['description']
-        date = request_data['date']
-        
-        event = Event(calendar_id = calendar_id,
-        title = title, description = description, date = date
-        )
-        event.save()
-        
-        return Response(True)
+    def delete(self, request, cal_id, event_id):
+        if request.method == 'DELETE':
+            event = Event.objects.filter(id=event_id).delete()
+            return Response("deleted")
 
-@api_view(['DELETE'])
-def eventDeleteAPI(request, cal_id, event_id):
-    if request.method == 'DELETE':
-        event = Event.objects.filter(id=event_id).delete()
-        
-        print(event)
-        return Response("deleted")
+    def put(self, request, cal_id, event_id):
+        if request.method == 'PUT':
+            
+            event = Event.objects.get(id=event_id)
+            
+            request_data = json.loads(request.body.decode('utf-8'))[0]
+            event.title = request_data['title']
+            event.description = request_data['description']
+            event.date = request_data['date']
+            
+            event.save()
+
+            return Response(True)
